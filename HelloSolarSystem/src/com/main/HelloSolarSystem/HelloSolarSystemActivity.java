c4f674f647f9e559dbf16fcc99a46dbc3151f5e5
Erik Thomassen <pinmedrnietopvast@gmail.com>
2012-03-28 21:12:49 +0200

    finally got it to work in the emulator!

--- HelloSolarSystem/src/com/main/HelloSolarSystem/HelloSolarSystemActivity.java
+++ HelloSolarSystem/src/com/main/HelloSolarSystem/HelloSolarSystemActivity.java
@@ -1,44 +1,55 @@
 package com.main.HelloSolarSystem; //File name and location within file structure
 
+import android.R.string;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.*;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 // import android.widget.RadioButton; //Only required if radio buttons are needed
 
 public class HelloSolarSystemActivity extends Activity {
-	  private Button btnplus;
-	  private Button btnminus;
+	  private ImageButton btnplus;
+	  private ImageButton btnminus;
 	  private TextView tvnumber;
+	  private int NumberOnScreen = 0;
+	 
 	  //RadioButton myOption1, myOption2, myOption3; //Only required if radio buttons are needed
 	
 ///////// 
 //Section goal - Connect PLUS and MINUS buttons in UI so they control the number value in the center of the screen
 /////////
 	  
+	 //TODO Create a personal toast. http://www.androidpeople.com/android-custom-toast-tutorial 
 	@Override
     public void onCreate(Bundle savedInstanceState) { 			// I do not know what this does
 	      super.onCreate(savedInstanceState); 					// I do not know what this does
 	      setContentView(R.layout.main); 						// Connect this file to the interface xml file (in general)
-	      btnplus = (Button)findViewById(R.id.plus);			// Connect UI element in the interface file to a specific variable in this file
-	      btnminus = (Button)findViewById(R.id.minus);			// Connect UI element in the interface file to a specific variable in this file
+	      btnplus = (ImageButton)findViewById(R.id.plus);		// Connect UI element in the interface file to a specific variable in this file
+	      btnminus = (ImageButton)findViewById(R.id.minus);		// Connect UI element in the interface file to a specific variable in this file
 	      tvnumber = (TextView)findViewById(R.id.number);		// Connect UI element in the interface file to a specific variable in this file
+	      	      
 	      btnplus.setOnClickListener(new OnClickListener() {	// Add event handler to buttons in UI
-	    	  	public void onClick(View v) {
-	    		  Toast.makeText(HelloSolarSystemActivity.this, "plus button pressed", Toast.LENGTH_SHORT).show(); // Show pop-up to verify it works
-	    		  tvnumber.setText("99");						// Update value in center of screen with high number 
-	    		  //TODO replace static text dynamic connection UI buttons
+	      
+	    	  public void onClick(View v) {
+	    		  //Toast.makeText(HelloSolarSystemActivity.this, "plus button pressed", Toast.LENGTH_SHORT).show(); // Show pop-up to verify it works
+	    		  String interum = tvnumber.getText().toString();
+	    		  NumberOnScreen = Integer.valueOf(interum) + 1;  
+	    		  tvnumber.setText(Integer.toString(NumberOnScreen));
 	    	  }
 	      });
 	      
+	      
 	      btnminus.setOnClickListener(new OnClickListener() {	// Add event handler to buttons in UI
 	    	  	public void onClick(View v) {
-	    		  Toast.makeText(HelloSolarSystemActivity.this, "minus button pressed", Toast.LENGTH_SHORT).show(); // Show pop-up to verify it works
-	    		  tvnumber.setText("00");						// Update value in center of screen with high number 
-	    		  //TODO replace static text dynamic connection UI buttons
+	    		  //Toast.makeText(HelloSolarSystemActivity.this, "minus button pressed", Toast.LENGTH_SHORT).show(); // Show pop-up to verify it works
+	    		  String interum = tvnumber.getText().toString();
+	    		  NumberOnScreen = Integer.valueOf(interum) - 1;  
+	    		  tvnumber.setText(Integer.toString(NumberOnScreen));
 	    	  }
 	      });
 	}
