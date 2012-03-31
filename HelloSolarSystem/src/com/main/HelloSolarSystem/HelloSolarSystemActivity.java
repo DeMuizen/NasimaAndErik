1473fe3a7e49a08f619336843af74079fdeae946
Erik Thomassen <pinmedrnietopvast@gmail.com>
2012-03-31 22:13:08 +0200

    Solved bug #1 Variable name consistency

--- HelloSolarSystem/src/com/main/HelloSolarSystem/HelloSolarSystemActivity.java
+++ HelloSolarSystem/src/com/main/HelloSolarSystem/HelloSolarSystemActivity.java
@@ -1,206 +1,102 @@
-//TODO Create a personal toast. http://www.androidpeople.com/android-custom-toast-tutorial 
-
 package com.main.HelloSolarSystem; //File name and location within file structure
 
-import android.R.string;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.*;
 import android.widget.AdapterView;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
-// import android.widget.RadioButton; //Only required if radio buttons are needed
 
 public class HelloSolarSystemActivity extends Activity {
-	private ImageButton btnplusleft;
-	private ImageButton btnplusright;
-	private ImageButton btnminusleft;
-	private ImageButton btnminusright;
-	private TextView tvnumberleft;
-	private TextView tvnumberright;
-	private TextView tvanswer;
-	private int NumberOnScreenleft = 35;
-	private int NumberOnScreenright = 35;
+	private ImageButton btnPlusLeft;
+	private ImageButton btnPlusRight;
+	private ImageButton btnMinusLeft;
+	private ImageButton btnMinusRight;
+	private TextView tvNumberLeft;
+	private TextView tvNumberRight;
+	private TextView tvAnswer;
+	private int NumberOnScreenLeft = 10;
+	private int NumberOnScreenRight = 10;
 	public boolean FirstLoad = true;
 	private String SpinnerChoice = "x";
-	  
-	//RadioButton myOption1, myOption2, myOption3; //Only required if radio buttons are needed
-		 	  
+	  		 	  
 	@Override
     public void onCreate(Bundle savedInstanceState) { 					// I do not know what this does
 	      super.onCreate(savedInstanceState); 							// I do not know what this does
 	      setContentView(R.layout.main); 								// Connect this file to the interface xml file (in general)
-	      btnplusleft = (ImageButton)findViewById(R.id.plusleft);		// Connect UI element in the interface file to a specific variable in this file
-	      btnplusright = (ImageButton)findViewById(R.id.plusright);		// Connect UI element in the interface file to a specific variable in this file
-	      btnminusleft = (ImageButton)findViewById(R.id.minusleft);		// Connect UI element in the interface file to a specific variable in this file
-	      btnminusright = (ImageButton)findViewById(R.id.minusright);	// Connect UI element in the interface file to a specific variable in this file
-	      tvnumberleft = (TextView)findViewById(R.id.numberleft);		// Connect UI element in the interface file to a specific variable in this file
-	      tvnumberright = (TextView)findViewById(R.id.numberright);		// Connect UI element in the interface file to a specific variable in this file
-	      tvanswer = (TextView)findViewById(R.id.answer);				// Connect UI element in the interface file to a specific variable in this file
-	      final Spinner spinner = (Spinner) findViewById(R.id.spinner);
+	      btnPlusLeft = (ImageButton)findViewById(R.id.PlusLeft);		// Connect UI element in the interface file to a specific variable in this file
+	      btnPlusRight = (ImageButton)findViewById(R.id.PlusRight);		// Connect UI element in the interface file to a specific variable in this file
+	      btnMinusLeft = (ImageButton)findViewById(R.id.MinusLeft);		// Connect UI element in the interface file to a specific variable in this file
+	      btnMinusRight = (ImageButton)findViewById(R.id.MinusRight);	// Connect UI element in the interface file to a specific variable in this file
+	      tvNumberLeft = (TextView)findViewById(R.id.NumberLeft);		// Connect UI element in the interface file to a specific variable in this file
+	      tvNumberRight = (TextView)findViewById(R.id.NumberRight);		// Connect UI element in the interface file to a specific variable in this file
+	      tvAnswer = (TextView)findViewById(R.id.Answer);				// Connect UI element in the interface file to a specific variable in this file
+	      final Spinner Spinner = (Spinner) findViewById(R.id.Spinner);
 	      
 	      //Spinner event handler
-		  spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+		  Spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
 		      public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-		          //if(FirstLoad){
-		          //        FirstLoad = false;
-		          //        return;                         
-		          //}
-		          SpinnerChoice = spinner.getItemAtPosition(i).toString();                 
-		          UpdateAnswer();
-				  //if (SpinnerChoice.toString().equals("+") ) {
-				//	  Toast.makeText(HelloSolarSystemActivity.this, "String compare works", Toast.LENGTH_SHORT).show(); 
-				//	  UpdateAnswer();
-				//  }
-		      
+		          SpinnerChoice = Spinner.getItemAtPosition(i).toString();                 
+		          UpdateAnswer();		      
 		      } // end of method
 		      public void onNothingSelected(AdapterView<?> adapterView) {
 		          return;
 		      } // end of method 
 		  }); //end of event handler
 	      		  
-	     btnplusleft.setOnClickListener(new OnClickListener() {	// Add event handler to buttons in UI
+	     btnPlusLeft.setOnClickListener(new OnClickListener() {	// Add event handler to buttons in UI
 	    	  public void onClick(View v) {
-	    		  //Toast.makeText(HelloSolarSystemActivity.this, "plus button pressed", Toast.LENGTH_SHORT).show(); // Show pop-up to verify it works
-	    		  String interumleft = tvnumberleft.getText().toString();
-	    		  NumberOnScreenleft = Integer.valueOf(interumleft) + 1;  
-	    		  tvnumberleft.setText(Integer.toString(NumberOnScreenleft));
+	    		  String InterumLeft = tvNumberLeft.getText().toString();
+	    		  NumberOnScreenLeft = Integer.valueOf(InterumLeft) + 1;  
+	    		  tvNumberLeft.setText(Integer.toString(NumberOnScreenLeft));
 	    		  UpdateAnswer();
-	    		  //tvanswer.setText(Integer.toString(NumberOnScreenright * NumberOnScreenleft));
 	    	  } // end of method
 	      }); //end of event handler
 	      
-	      btnminusleft.setOnClickListener(new OnClickListener() {	// Add event handler to buttons in UI
+	      btnMinusLeft.setOnClickListener(new OnClickListener() {	// Add event handler to buttons in UI
 	    	  	public void onClick(View v) {
-	    		  //Toast.makeText(HelloSolarSystemActivity.this, "minus button pressed", Toast.LENGTH_SHORT).show(); // Show pop-up to verify it works
-	    		  String interumleft = tvnumberleft.getText().toString();
-	    		  NumberOnScreenleft = Integer.valueOf(interumleft) - 1;  
-	    		  tvnumberleft.setText(Integer.toString(NumberOnScreenleft));
+	    		  String InterumLeft = tvNumberLeft.getText().toString();
+	    		  NumberOnScreenLeft = Integer.valueOf(InterumLeft) - 1;  
+	    		  tvNumberLeft.setText(Integer.toString(NumberOnScreenLeft));
 	    		  UpdateAnswer();
-	    		  //tvanswer.setText(Integer.toString(NumberOnScreenright * NumberOnScreenleft));
 	    	  } // end of method
 	      }); //end of event handler
 	      
-	      btnplusright.setOnClickListener(new OnClickListener() {	// Add event handler to buttons in UI
+	      btnPlusRight.setOnClickListener(new OnClickListener() {	// Add event handler to buttons in UI
 		      
 	    	  public void onClick(View v) {
-	    		  //Toast.makeText(HelloSolarSystemActivity.this, "plus button pressed", Toast.LENGTH_SHORT).show(); // Show pop-up to verify it works
-	    		  String interumright = tvnumberright.getText().toString();
-	    		  NumberOnScreenright = Integer.valueOf(interumright) + 1;  
-	    		  tvnumberright.setText(Integer.toString(NumberOnScreenright));
+	    		  String InterumRight = tvNumberRight.getText().toString();
+	    		  NumberOnScreenRight = Integer.valueOf(InterumRight) + 1;  
+	    		  tvNumberRight.setText(Integer.toString(NumberOnScreenRight));
 	    		  UpdateAnswer();
-	    		  //tvanswer.setText(Integer.toString(NumberOnScreenright * NumberOnScreenleft));
 	    	  } // end of method
 	      }); //end of event handler
 	      
-	      btnminusright.setOnClickListener(new OnClickListener() {	// Add event handler to buttons in UI
+	      btnMinusRight.setOnClickListener(new OnClickListener() {	// Add event handler to buttons in UI
 	    	  	public void onClick(View v) {
-	    		  //Toast.makeText(HelloSolarSystemActivity.this, "minus button pressed", Toast.LENGTH_SHORT).show(); // Show pop-up to verify it works
-	    		  String interumright = tvnumberright.getText().toString();
-	    		  NumberOnScreenright = Integer.valueOf(interumright) - 1;  
-	    		  tvnumberright.setText(Integer.toString(NumberOnScreenright));
+	    		  String InterumRight = tvNumberRight.getText().toString();
+	    		  NumberOnScreenRight = Integer.valueOf(InterumRight) - 1;  
+	    		  tvNumberRight.setText(Integer.toString(NumberOnScreenRight));
 	    		  UpdateAnswer();
-	    		  //tvanswer.setText(Integer.toString(NumberOnScreenright * NumberOnScreenleft));
 	    	  } // end of method
 	      }); //end of event handler
-
 	} //end of OnCreate Bundle method      
 
     public void UpdateAnswer(){
 		if (SpinnerChoice.toString().equals("add") ) { 
-		      tvanswer.setText(Integer.toString(NumberOnScreenright + NumberOnScreenleft));  
+		      tvAnswer.setText(Integer.toString(NumberOnScreenRight + NumberOnScreenLeft));  
 	    } else if (SpinnerChoice.toString().equals("subtract") ) {
-			  tvanswer.setText(Integer.toString(NumberOnScreenright - NumberOnScreenleft));  
+			  tvAnswer.setText(Integer.toString(NumberOnScreenLeft - NumberOnScreenRight));  
 		} else if (SpinnerChoice.toString().equals("multiply") ) {
-			  tvanswer.setText(Integer.toString(NumberOnScreenright * NumberOnScreenleft));
+			  tvAnswer.setText(Integer.toString(NumberOnScreenRight * NumberOnScreenLeft));
 	    } else if (SpinnerChoice.toString().equals("divide") ) {
-			  if ((NumberOnScreenright == 0) || (NumberOnScreenleft ==0)) {
-				  tvanswer.setText(Integer.toString(0));
+			  if ((NumberOnScreenRight == 0) || (NumberOnScreenLeft ==0)) {
+				  tvAnswer.setText(Integer.toString(0));
 			  } else {
-				  tvanswer.setText(Integer.toString(NumberOnScreenright / NumberOnScreenleft));
+				  tvAnswer.setText(Integer.toString(NumberOnScreenLeft / NumberOnScreenRight));
 			  }
 		 }	
 	} //end of method
-
 } //end of Hello SolarSystem class/file
-
-
-
-
-
-///////// 
-//Section goal - Previous exercises I'm keeping so I can lookup code solutions lateron
-/////////
-
-/*      
-	      //get the two controls we created earlier, also with the resource reference and the id
-	      final TextView tv_View = (TextView)findViewById(R.id.tv_View);
-	      final EditText et_Text = (EditText)findViewById(R.id.et_Text);
-	          
-	      //add new KeyListener Callback (to record key input)
-	      et_Text.setOnClickListener(new OnClickListener()
-	      {
-	          //function to invoke when a key is pressed
-	          public boolean onKey(View v, int keyCode, KeyEvent event)
-	          {
-	              //check if there is 
-	              if (event.getAction() == KeyEvent.ACTION_DOWN)
-	              {
-	                  //check if the right key was pressed
-	                  if (keyCode == KeyEvent.KEYCODE_DPAD_CENTER)
-	                  {
-	                                      //add the text to the textview
-	                      tv_View.setText(tv_View.getText() + ", " + 
-	                                       et_Text.getText());
-	                                      //and clear the EditText control
-	                      et_Text.setText("");
-	                      return true;
-	                  }
-	              }
-	              return false;
-	          }
-			public void onClick(View v) {
-				// TODO Auto-generated method stub	
-			}
-	      });
-			
-		//super.onCreate(savedInstanceState);
-        //setContentView(R.layout.main);
-        myOption1 = (RadioButton)findViewById(R.id.option1);
-        myOption2 = (RadioButton)findViewById(R.id.option2);
-        myOption3 = (RadioButton)findViewById(R.id.option3);
-        myOption1.setOnClickListener(myOptionOnClickListener);
-        myOption2.setOnClickListener(myOptionOnClickListener);
-        myOption3.setOnClickListener(myOptionOnClickListener);
-        myOption1.setChecked(true);
-    }
-
- 	RadioButton.OnClickListener myOptionOnClickListener =
-    	new RadioButton.OnClickListener()
-    	{
-    		  public void onClick(View v) {
-    		   	// TODO Auto-generated method stub
-    		   	Toast.makeText(HelloSolarSystemActivity.this,
-    		     	"Option 1 : " + myOption1.isChecked() + "\n" +
-    		     	"Option 2 : " + myOption2.isChecked() + "\n" +
-    		     	"Option 3 : " + myOption3.isChecked(),
-    		     	Toast.LENGTH_LONG
-    		    ).show();
-    		   
-    		    Toast eriksToast = new Toast(HelloSolarSystemActivity.this);
-    		   		eriksToast.setDuration(5000);
-    		   		eriksToast.setText(
-    				"Option 1 : " + myOption1.isChecked() + "\n" +	
-    		    	"Option 2 : " + myOption2.isChecked() + "\n" +
-    		    	"Option 3 : " + myOption3.isChecked() 
-    		    	);
-    		    eriksToast.show();
-    		  }
-    	}; 		 
-}
-*/
